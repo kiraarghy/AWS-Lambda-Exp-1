e72caed40ba98704bf1b3b757d6b9c4015824fd0
kara <kara@moo.com>
2018-09-26 16:48:55 +0100

    Lambda Experiment - Refactor + Readme
    * Refactored index.js -> query.js and index.js
    * Added Readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# AWS Lambda experiment 1
+
+Task: Build AWS Lambda that queried a PSQL database + returned JSON to the endpoint. 
+
+## Running
+
+```
+npm run predeploy
+```
+
+Outputs a .zip to upload to AWS Lambda dashboard. 
+
+## Tests
+
+TODO
--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-require("dotenv").config();
-function query1() {
-	console.log("hit here");
-	var knex = require("knex")({
-		client: "pg",
-		connection: {
-			host: process.env.HOST,
-			user: process.env.USERNAME,
-			password: process.env.PASSWORD,
-			database: "postgres"
-		}
-	});
-
-	return knex
-		.select()
-		.from("photos")
-		.then(data => data);
-}
-
-function query2() {
-	var knex = require("knex")({
-		client: "pg",
-		connection: {
-			host: process.env.HOST,
-			user: process.env.USERNAME,
-			password: process.env.PASSWORD,
-			database: "postgres"
-		}
-	});
-
-	return knex
-		.select()
-		.from("content")
-		.then(data => data);
-}
+const query = require("./query");
 
 exports.handler = async function(event, callback) {
 	const response = {
 		statusCode: 200,
 		body: {
-			photos: await query1(),
-			content: await query2()
+			photos: await query("postgres", "photos"),
+			content: await query("postgres", "content")
 		}
 	};
 	return response;
